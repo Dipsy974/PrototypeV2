1f5cc39d3fe927726f32613d29b14016cefd7f12
RENNES\o.leclerc <orion.leclerc@gmail.com>
2023-10-13 15:55:47 +0200

    State machine interface

--- Assets/Scripts/StateMachine/InputStateMachine/StM_InputReader.cs
+++ Assets/Scripts/StateMachine/InputStateMachine/StM_InputReader.cs
@@ -13,6 +13,8 @@ public class StM_InputReader : ScriptableObject, PlayerInputActions.ICharacterCo
     public event UnityAction<Vector2, bool> Look = delegate {  }; 
     public event UnityAction EnableMouseControlCamera = delegate {  };
     public event UnityAction DisableMouseControlCamera = delegate {  };
+    public event UnityAction<bool> Jump = delegate {  };
+    
 
     private PlayerInputActions _inputActions;
 
@@ -53,6 +55,19 @@ public class StM_InputReader : ScriptableObject, PlayerInputActions.ICharacterCo
                 break;
             case InputActionPhase.Canceled:
                 DisableMouseControlCamera.Invoke();
+                break;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        switch (context.phase)
+        {
+            case InputActionPhase.Started:
+                Jump.Invoke(true);
+                break;
+            case InputActionPhase.Canceled:
+                Jump.Invoke(false);
                 break;
         }
     }
--- Assets/Scripts/StateMachine/StM_PlayerController.cs
+++ Assets/Scripts/StateMachine/StM_PlayerController.cs
@@ -13,17 +13,28 @@ public class StM_PlayerController : MonoBehaviour
     [SerializeField] private CinemachineFreeLook _freeLookCam;
     [SerializeField] private StM_InputReader _input;
 
-    [Header("Parameters")] 
+    [Header("Movement Parameters")] 
     [SerializeField] private float _moveSpeed = 6f;
     [SerializeField] private float _rotationSpeed = 15f;
     [SerializeField] private float _smoothTime = 0.1f;
+    
+    [Header("Jump Parameters")] 
+    [SerializeField] private float _jumpForce = 10f;
+    [SerializeField] private float _jumpDuration = 0.5f;
+    [SerializeField] private float _jumpCooldown = 0f;
+    [SerializeField] private float _jumpMaxHeight = 2f;
+    [SerializeField] private float _gravityMultiplier = 3f;
 
     private Transform mainCam;
 
     private const float ZeroF = 0f;
-    private float _currentSpeed, _velocity;
+    private float _currentSpeed, _velocity, _jumpVelocity;
 
     private Vector3 _movement;
+
+    private List<Timer> _timers;
+    private CountdownTimer _jumpTimer;
+    private CountdownTimer _jumpCooldownTimer;
 
     private void Awake()
     {
@@ -35,6 +46,13 @@ public class StM_PlayerController : MonoBehaviour
         _freeLookCam.OnTargetObjectWarped(transform, transform.position - _freeLookCam.transform.position - Vector3.forward);
 
         _rigidbody.freezeRotation = true;
+        
+        //Timers setup
+        _jumpTimer = new CountdownTimer(_jumpDuration);
+        _jumpCooldownTimer = new CountdownTimer(_jumpCooldown);
+        _timers = new List<Timer> { _jumpTimer, _jumpCooldownTimer };
+
+        _jumpTimer.OnTimerStop += () => _jumpCooldownTimer.Start();
     }
 
     private void Start()
@@ -42,14 +60,73 @@ public class StM_PlayerController : MonoBehaviour
         _input.EnablePlayerActions();
     }
 
+    private void OnEnable()
+    {
+        _input.Jump += OnJump;
+    }
+
+    private void OnDisable()
+    {
+        _input.Jump += OnJump;
+    }
+
+    void OnJump(bool performed)
+    {
+        if (performed && !_jumpTimer.IsRunning && !_jumpCooldownTimer.IsRunning && _groundCheck.IsGrounded)
+        {
+            _jumpTimer.Start();
+        }
+        else if (!performed && _jumpTimer.IsRunning)
+        {
+            _jumpTimer.Stop();
+        }
+        
+    }
+    
     private void Update()
     {
         _movement = new Vector3(_input.Direction.x, 0f, _input.Direction.y);
+
+        HandleTimers();
     }
 
     private void FixedUpdate()
     {
+        HandleJump();
         HandleMovement();
+    }
+
+    private void HandleTimers()
+    {
+        foreach (var timer in _timers)
+        {
+            timer.Tick(Time.deltaTime);
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (!_jumpTimer.IsRunning && _groundCheck.IsGrounded)
+        {
+            _jumpVelocity = ZeroF;
+            _jumpTimer.Stop();
+            return;
+        }
+
+        if (_jumpTimer.IsRunning)
+        {
+            float launchPoint = 0.9f;
+            if (_jumpTimer.Progress > launchPoint)
+            {
+                _jumpVelocity = Mathf.Sqrt((2 * _jumpMaxHeight * Mathf.Abs(Physics.gravity.y)));
+            }
+            else
+            {
+                _jumpVelocity += Physics.gravity.y * _gravityMultiplier * Time.fixedDeltaTime;
+            }
+
+            _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, _jumpVelocity, _rigidbody.velocity.z);
+        }
     }
 
     private void HandleMovement()
