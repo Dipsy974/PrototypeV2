2542795d6010e761cd64a9de15dde012825c4900
RENNES\o.leclerc <orion.leclerc@gmail.com>
2023-11-13 13:01:07 +0100

    target system suite

--- Assets/Scripts/RD/Sidekick/Target.cs
+++ Assets/Scripts/RD/Sidekick/Target.cs
@@ -6,10 +6,43 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     [SerializeField] private TargetSystem _targetSystem;
+    [SerializeField] private Transform _playerTransform;
+    public bool isReachable;
 
+    private Camera _mainCam;
     private void OnEnable()
     {
         _targetSystem = FindObjectOfType<TargetSystem>();
+        _playerTransform = FindObjectOfType<StM_PlayerController>().transform;
+    }
+
+    private void Start()
+    {
+        _mainCam = Camera.main;
+    }
+
+    private void Update()
+    {
+        Vector3 playerToObject = transform.position - _playerTransform.position;
+        bool isBehindPlayer = Vector3.Dot(_mainCam.transform.forward, playerToObject) < 0;
+
+        if (Vector3.Distance(transform.position, _playerTransform.position) < _targetSystem.minReachDistance && !isBehindPlayer && !isReachable)
+        {
+            isReachable = true;
+            if (_targetSystem.visibleTargets.Contains(this))
+            {
+                _targetSystem.reachableTargets.Add(this);
+            }
+        }
+
+        if ((Vector3.Distance(transform.position, _playerTransform.position) > _targetSystem.minReachDistance || isBehindPlayer) && isReachable)
+        {
+            isReachable = false;
+            if (_targetSystem.reachableTargets.Contains(this))
+            {
+                _targetSystem.reachableTargets.Remove(this);
+            }
+        }
     }
 
     private void OnBecameVisible()
@@ -17,6 +50,9 @@ public class Target : MonoBehaviour
         if (!_targetSystem.visibleTargets.Contains(this))
         {
             _targetSystem.visibleTargets.Add(this);
+            
+            if(isReachable)
+                _targetSystem.reachableTargets.Add(this);
         }
     }
 
@@ -25,6 +61,11 @@ public class Target : MonoBehaviour
         if (_targetSystem.visibleTargets.Contains(this))
         {
             _targetSystem.visibleTargets.Remove(this);
+            
+            if (_targetSystem.reachableTargets.Contains(this))
+            {
+                _targetSystem.reachableTargets.Remove(this);
+            }
         }
     }
 }
--- Assets/Scripts/RD/Sidekick/TargetSystem.cs
+++ Assets/Scripts/RD/Sidekick/TargetSystem.cs
@@ -5,5 +5,14 @@ using UnityEngine;
 public class TargetSystem : MonoBehaviour
 {
     public List<Target> visibleTargets;
+    public List<Target> reachableTargets;
+    public float minReachDistance = 70;
+    
+    public Target currentTarget;
+    public Target storedTarget;
+    
+    [SerializeField] float _screenDistanceWeight = 1;
+    [SerializeField] float _positionDistanceWeight = 8;
+    
     
 }
